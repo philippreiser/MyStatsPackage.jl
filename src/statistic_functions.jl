0da36fe80d0e8d5f716372ba82b71c27871e463a
philippreiser <philipp@fam-reiser.eu>
2023-10-11 10:42:48 +0200

    add rse_mean, std, tstat docs

--- src/statistic_functions.jl
+++ src/statistic_functions.jl
@@ -36,6 +36,30 @@ rse_sum(1:36) == 666
 #---
 
 #---
+"""
+rse_mean(elements)::Real
+
+Return the mean of a list of elements.
+
+# Arguments
+
+- `elements` -- List of elements
+
+# Output
+
+A real number representing the mean.
+
+# Notes
+
+The mean is computed as ∑.
+
+# Examples
+
+```julia-repl
+julia> rse_mean([1, 2, 3])
+2.0
+\```
+"""
 function rse_mean(elements)
     rse_mean_result = 1 / length(elements) * rse_sum(elements)
     return rse_mean_result
@@ -44,6 +68,30 @@ rse_mean(-15:17) == 1
 #---
 
 #---
+"""
+rse_std(elements)::Real
+
+Return the standard deviation of a list of elements.
+
+# Arguments
+
+- `elements` -- List of elements
+
+# Output
+
+A real number representing the mean.
+
+# Notes
+
+The mean is computed over all elements.
+
+# Examples
+
+```julia-repl
+julia> rse_std([1, 2, 3])
+1.0
+\```
+"""
 function rse_std(elements)
     rse_std_result =
         sqrt(sum((elements .- rse_mean(elements)) .^ 2) / (length(elements) - 1))
@@ -53,6 +101,30 @@ rse_std(1:3) == 1
 #---
 
 #---
+"""
+rse_tstat(elements)::Real
+
+Return the t-statistic of a list of elements.
+
+# Arguments
+
+- `elements` -- List of elements
+
+# Output
+
+A real number representing the t-statistic.
+
+# Notes
+
+The t-statisitc is computed over as .
+
+# Examples
+
+```julia-repl
+julia> rse_std([1, 2, 3])
+1.0
+\```
+"""
 function rse_tstat(elements, σ = rse_std(elements))
     rse_mean(elements) / (σ / sqrt(length(elements)))
 end
